bbfc095e4d8c5459661934d33fccb00cc82ef806
FelipeMoura <felipe.rosa@dotgroup.com.br>
2015-12-28 21:45:51 -0200

    alteração rodapé

--- resources/views/admin-lte/footer.blade.php
+++ resources/views/admin-lte/footer.blade.php
@@ -2,5 +2,5 @@
 <footer class="main-footer">
     <!-- To the right -->
     <!-- Default to the left -->
-    <strong>ADMIN © 2015 Todos os direitos reservados a MH.
+    <strong>iSell © 2015 Todos os direitos reservados a iSell.
 </footer>
