7fa825f00343f75da26b6b2882576db67b4eb8f1
felipemouradev <felipemouracv@gmail.com>
2016-01-27 13:29:27 -0200

    Update app.php

--- config/app.php
+++ config/app.php
@@ -155,7 +155,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Way\Generators\GeneratorsServiceProvider::class,
+        //Way\Generators\GeneratorsServiceProvider::class,
         Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class
 
     ],
