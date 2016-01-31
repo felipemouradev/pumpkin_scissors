1d2785a92ecd081f75d0b06e2103a0371669620a
felipemoura <felipemouracv@gmail.com>
2016-01-31 00:25:14 -0200

    View listClipping

--- resources/views/sistemas/clippings/listClippings.blade.php
+++ resources/views/sistemas/clippings/listClippings.blade.php
@@ -15,38 +15,39 @@
 
         <table class='table table-bordered'>
           <tbody>
-            <tr><th style='width: 10px'>Código</th>
-<th>Data_clipping</th>
-<th>Centimetragem</th>
-<th>Type</th>
-<th>Jornal_id</th>
-<th>Editoria_id</th>
-<th>Fonte_id</th>
-<th>Status_id</th>
-<th>Usuario_id</th>
-<th>Cliente_id</th>
-<th>Ações</th>
+            <tr>
+              <th style='width: 10px'>Código</th>
+              <th>Data_clipping</th>
+              <th>Centimetragem</th>
+              <th>Type</th>
+              <th>Jornal</th>
+              <th>Editoria</th>
+              <th>Fonte</th>
+              <th>Status</th>
+              <th>Cliente</th>
+              <th>Salvo por: </th>
+              <th>Ações</th>
+            </tr>
         </tbody>
         @foreach ($data as $k )
-            <tr><td>{{ $k->id }}</td>
-<td>{{ $k->data_clipping }}</td>
-<td>{{ $k->centimetragem }}</td>
-<td>{{ $k->type }}</td>
-<td>{{ $k->jornal_id }}</td>
-<td>{{ $k->editoria_id }}</td>
-<td>{{ $k->fonte_id }}</td>
-<td>{{ $k->status_id }}</td>
-<td>{{ $k->usuario_id }}</td>
-<td>{{ $k->cliente_id }}</td>
-
-              <td>
-                  <a href='/admin/clipping/{{ $k->id }}'><span class='badge bg-green'>Ver</span></a>
-                  <a href='/admin/clipping/{{ $k->id }}/editar'><span class='badge bg-yellow'>Editar</span></a>
-                  <a href='/admin/clipping/{{ $k->id }}/deletar'><span class='badge bg-red'>Excluir</span></a>
-
-              </td>
-            </tr>
-            @endforeach
+        <tr>
+          <td>{{ $k->id }}</td>
+          <td>{{ $k->data_clipping }}</td>
+          <td>{{ $k->centimetragem }}</td>
+          <td>{{ $k->type }}</td>
+          <td>{{ $k->jornal_id }}</td>
+          <td>{{ $k->editoria->nome }}</td>
+          <td>{{ $k->fonte->nome }}</td>
+          <td>{{ $k->status->nome }}</td>
+          <td>{{ $k->cliente->nome }}</td>
+          <td>{{ $k->usuario->nome }}</td>
+          <td>
+            <a href='/admin/clipping/{{ $k->id }}'><span class='badge bg-green'>Ver</span></a>
+            <a href='/admin/clipping/{{ $k->id }}/editar'><span class='badge bg-yellow'>Editar</span></a>
+            <a href='/admin/clipping/{{ $k->id }}/deletar'><span class='badge bg-red'>Excluir</span></a>
+          </td>
+        </tr>
+        @endforeach
         </tbody>
         </table>
     </div>
