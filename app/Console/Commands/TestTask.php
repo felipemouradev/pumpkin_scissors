36237f4112c20f97208644496c16c09fbebc5f2c
FelipeMoura <felipe.rosa@dotgroup.com.br>
2016-02-07 11:21:46 -0200

    Teste Cron

---
+++ app/Console/Commands/TestTask.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TestTask extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'testtask';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+        $teste = "isso é um teste ".date('Y-m-d H:m:s')."\n";
+        file_put_contents(public_path()."/teste".time().".txt",$teste.FILE_APPEND);
+    }
+}
